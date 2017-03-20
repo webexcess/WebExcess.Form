9cbcb692d380165d9f3a83e2c634f6f00bd351d6
Samuel Hauser <samuel.hauser@dotpulse.ch>
2017-03-20 15:03:28 +0100

    Update attachment finisher

--- Classes/WebExcess/Form/Finishers/EmailWithAttachmentFinisher.php
+++ Classes/WebExcess/Form/Finishers/EmailWithAttachmentFinisher.php
@@ -1,17 +1,25 @@
 <?php
 namespace WebExcess\Form\Finishers;
 
-    /*                                                                        *
-     * This script belongs to the TYPO3 Flow package "TYPO3.Form".            *
-     *                                                                        *
-     * It is free software; you can redistribute it and/or modify it under    *
-     * the terms of the GNU Lesser General Public License, either version 3   *
-     * of the License, or (at your option) any later version.                 *
-     *                                                                        *
-     * The TYPO3 project - inspiring people to share!                         *
-     *                                                                        */
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Form\Core\Model\AbstractFinisher;
+use TYPO3\Form\Exception\FinisherException;
+use TYPO3\Fluid\View\StandaloneView;
+use TYPO3\SwiftMailer;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use WebExcess\Form\Service\FormHelperService;
 
-class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinisher
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Form".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+class EmailWithAttachmentFinisher extends AbstractFinisher
 {
     const FORMAT_PLAINTEXT = 'plaintext';
     const FORMAT_HTML = 'html';
@@ -20,21 +28,27 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
      * @var array
      */
     protected $defaultOptions = array(
-        'recipientAddressFallback' => '',
         'recipientName' => '',
         'senderName' => '',
         'excludeFields' => array(),
         'attachmentFields' => array(),
         'format' => self::FORMAT_HTML,
         'testMode' => false,
+        'debugMessage' => false,
     );
+
+    /**
+     * @Flow\Inject
+     * @var FormHelperService
+     */
+    protected $formHelperService;
 
     /**
      * Executes this finisher
      * @see AbstractFinisher::execute()
      *
      * @return void
-     * @throws \TYPO3\Form\Exception\FinisherException
+     * @throws FinisherException
      */
     protected function executeInternal()
     {
@@ -56,21 +70,46 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
         $arguments = $formRuntime->getRequest()->getArguments();
         $argumentsTmp = array();
         foreach ($arguments as $argument => $value) {
-            if ( !in_array($argument, $excludeFields) && !in_array($argument, $attachmentFields) ) {
+            if (!in_array($argument, $excludeFields) && !in_array($argument, $attachmentFields)) {
                 $argumentsTmp[$argument] = $value;
             }
         }
         $formRuntime->getRequest()->setArguments($argumentsTmp);
 
+        $formValues = array();
+        foreach ($formRuntime->getRequest()->getArguments() as $key => $value) {
+            $element = $formRuntime->getFormDefinition()->getElementByIdentifier($key);
+
+            if ($element) {
+                $formValues[$key]['label'] = $this->formHelperService->getTranslatedLabel('label', $element);
+
+                if (is_array($value)) {
+                    foreach ($value as $valueItem) {
+                        $translatedValue = $this->formHelperService->getTranslatedLabel('options.' . $valueItem, $element);
+                        $formValues[$key]['value'][] = $translatedValue == '' ? nl2br($valueItem) : nl2br($translatedValue);
+                    }
+                } else {
+                    $translatedValue = $this->formHelperService->getTranslatedLabel('options.' . $value, $element);
+                    $formValues[$key]['value'] = $translatedValue == '' ? nl2br($value) : nl2br($translatedValue);
+                }
+            }
+        }
+
         $standaloneView = $this->initializeStandaloneView();
-        $standaloneView->assign('form', $formRuntime);
+        $standaloneView->assign('formValues', $formValues);
+        $standaloneView->assign('postValues', $formRuntime->getRequest()->getArguments());
         $message = $standaloneView->render();
 
+        if ($this->parseOption('debugMessage')) {
+            echo $message;
+            exit;
+        }
+
         $recipientAddress = null;
-        if ( $this->parseOption('recipientAddress') != '' ) {
+        if ($this->parseOption('recipientAddress') != '') {
             $recipientAddress = $this->parseOption('recipientAddress');
         } else {
-            if ( $this->parseOption('recipientAddressFallback') ) {
+            if ($this->parseOption('recipientAddressFallback')) {
                 $recipientAddress = $this->parseOption('recipientAddressFallback');
             }
         }
@@ -86,16 +125,16 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
         $testMode = $this->parseOption('testMode');
 
         if ($subject === null) {
-            throw new \TYPO3\Form\Exception\FinisherException('The option "subject" must be set for the EmailFinisher.', 1327060320);
+            throw new FinisherException('The option "subject" must be set for the EmailFinisher.', 1327060320);
         }
         if ($recipientAddress === null) {
-            throw new \TYPO3\Form\Exception\FinisherException('The option "recipientAddress" must be set for the EmailFinisher.', 1327060200);
+            throw new FinisherException('The option "recipientAddress" must be set for the EmailFinisher.', 1327060200);
         }
         if ($senderAddress === null) {
-            throw new \TYPO3\Form\Exception\FinisherException('The option "senderAddress" must be set for the EmailFinisher.', 1327060210);
+            throw new FinisherException('The option "senderAddress" must be set for the EmailFinisher.', 1327060210);
         }
 
-        $mail = new \TYPO3\SwiftMailer\Message();
+        $mail = new SwiftMailer\Message();
 
         $mail
             ->setFrom(array($senderAddress => $senderName))
@@ -127,33 +166,52 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
         }
 
         if ($testMode === true) {
-            \TYPO3\Flow\var_dump(
-                array(
-                    'sender' => array($senderAddress => $senderName),
-                    'recipient' => array($recipientAddress => $recipientName),
-                    'replyToAddress' => $replyToAddress,
-                    'carbonCopyAddress' => $carbonCopyAddress,
-                    'blindCarbonCopyAddress' => $blindCarbonCopyAddress,
-                    'message' => $message,
-                    'format' => $format,
-                ),
-                'E-Mail "' . $subject . '"'
-            );
+            echo '<table style="margin-bottom: 30px;">
+                <tr>
+                    <td><i>Subject:</i></td>
+                    <td>' . $subject . '</td>
+                </tr>
+                <tr>
+                    <td><i>Sender (Email => Name):</i></td>
+                    <td>' . $senderAddress . ' => ' . $senderName . '</td>
+                </tr>
+                <tr>
+                    <td><i>Recipient (Email => Name):</i></td>
+                    <td>' . $recipientAddress . ' => ' . $recipientName . '</td>
+                </tr>
+                <tr>
+                    <td><i>Reply to address:</i></td>
+                    <td>' . $replyToAddress . '</td>
+                </tr>
+                <tr>
+                    <td><i>Carbon copy address:</i></td>
+                    <td>' . $carbonCopyAddress . '</td>
+                </tr>
+                <tr>
+                    <td><i>Blind carbon copy address:</i></td>
+                    <td>' . $blindCarbonCopyAddress . '</td>
+                </tr>
+                <tr>
+                    <td><i>Format:</i></td>
+                    <td>' . $format . '</td>
+                </tr>
+            </table><hr style="margin-bottom: 30px;">';
+            echo $message;
+            exit;
         } else {
             $mail->send();
         }
     }
 
     /**
-     * @return \TYPO3\Fluid\View\StandaloneView
-     * @throws \TYPO3\Form\Exception\FinisherException
+     * @return StandaloneView
+     * @throws FinisherException
      */
     protected function initializeStandaloneView()
     {
-        //\TYPO3\Flow\var_dump($this->options);
-        $standaloneView = new \TYPO3\Fluid\View\StandaloneView();
+        $standaloneView = new StandaloneView();
         if (!isset($this->options['templatePathAndFilename'])) {
-            throw new \TYPO3\Form\Exception\FinisherException('The option "templatePathAndFilename" must be set for the EmailFinisher.', 1327058829);
+            throw new FinisherException('The option "templatePathAndFilename" must be set for the EmailFinisher.', 1327058829);
         }
         $standaloneView->setTemplatePathAndFilename($this->options['templatePathAndFilename']);
 
@@ -169,7 +227,6 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
             $standaloneView->assignMultiple($this->options['variables']);
         }
 
-        //\TYPO3\Flow\var_dump($standaloneView->getRequest()->getArguments());
         return $standaloneView;
     }
 
@@ -180,7 +237,8 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
      * @param string $optionName
      * @return mixed|string
      */
-    protected function parseOption($optionName) {
+    protected function parseOption($optionName)
+    {
         if (!isset($this->options[$optionName]) || $this->options[$optionName] === '') {
             if (isset($this->defaultOptions[$optionName])) {
                 $option = $this->defaultOptions[$optionName];
@@ -193,13 +251,14 @@ class EmailWithAttachmentFinisher extends \TYPO3\Form\Core\Model\AbstractFinishe
         if (!is_string($option)) {
             return $option;
         }
-        if(preg_match('/{node\.([^}]+)}/', $option, $matches)) {
+        if (preg_match('/{node\.([^}]+)}/', $option, $matches)) {
             $renderingOptions = $this->finisherContext->getFormRuntime()->getRenderingOptions();
-            if(isset($renderingOptions['node'])) {
+            if (isset($renderingOptions['node'])) {
+                /** @var NodeInterface $node */
                 $node = $renderingOptions['node'];
-                if($node->hasProperty($matches[1])) {
+                if ($node->hasProperty($matches[1])) {
                     $property = $node->getProperty($matches[1]);
-                    if(!empty($property)) {
+                    if (!empty($property)) {
                         return $property;
                     }
                 }
